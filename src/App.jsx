46cffb0fd086c22f8566b47b9fa5f5a2d8f48200
Lorenzo <lorenzo1997chierisini@gmail.com>
2024-12-19 16:02:11 +0100

    Discovering some features of React 18 syntax

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,23 @@ without having a physical footprint in the DOM.
 const App = () => {
   /* You can insert JS code here and use it in the single component below. */
   const name = 'John Doe';
+  const names = ['John', 'Doe', 'Jane', 'Doe'];
+  const loggedIn = true;
 
 
   return (
     <>
-    <div className='text-5xl'>App</div>
-    <p>Hello {name}</p>
+      <div className='text-5xl'>App</div>
+      {/* You can inject style properties inline with the below syntax */}
+      <p style={{color: 'red', fontSize: '24px', marginTop: '30px' }}>Hello {name}</p>
+      {/* You can use JS code here to create a loop on a list of names. */}
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+      {/* You can use JS code here to create a conditional statement */}
+      {loggedIn ? <p>Welcome back!</p> : <p>Please log in</p>}
     </>
   );
 };

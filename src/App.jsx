005752c0179d1dadb61cb5f65dcfa740cf7f21c1
Lorenzo <lorenzo1997chierisini@gmail.com>
2024-12-20 17:42:30 +0100

    Created Detail jobs page route

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import MainLayout from './Layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage from './pages/JobPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage /> } />  
     <Route path='jobs' element={<JobsPage />} />
+    <Route path='/jobs/:Id' element={<JobPage />} />
     <Route path='*' element={<NotFoundPage />} />
+
   </Route>
   )
 );
